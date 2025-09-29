6c64898b71a45fb250bc3495329af82ea8bcb344
jakegithubpage <164205961+jakegithubpage@users.noreply.github.com>
2025-09-28 20:52:20 -0400

    started adding functions to start, send and request session and prompt
    Started dealing with request and prompt handling within send_LLM(); also added additional send_Http_request(); in order to start session with open source LLM and request answer based on user input. (Pretty sure this is able to be simplified into only a send_LLM(); will come back and brainstorm that. Kind of want it to use a seperate request function for orginazation and readability with this project though!

--- LLMsandr.c
+++ LLMsandr.c
@@ -10,18 +10,68 @@ HINTERNET tsConnect = NULL;
 HINTERNET tsRequest = NULL;
 
 int init_http() {
-    hsession = WinHttpOpen(L"TonyGPT_V1.0",
+    tsSession = WinHttpOpen(L"TonyGPT_V1.0",
                            WINHTTP_ACCESS_TYPE_DEFAULT_PROXY,
                            WINHTTP_NO_PROXY_NAME,
                            WINHTTP_NO_PROXY_NAME, 0);
 
-    if (!hSession) {
-        printf("failedt o init sessions, sorry :(");
+    if (!tsSession) {
+        printf("failed to init sessions, sorry cock sucka :(");
         return 0;
     }
-    else{
-        return 1;
+    
+}
+
+void cleanHTTP(){
+    if (tsRequest) WinHttpCloseHandle(tsRequest);
+    if (tsConnect) WinHttpCloseHandle(tsConnect);
+    if (tsSession) WinHttpCloseHandle(tsSession);
+}
+
+
+void send_http_request(char* json_data){
+    tsConnect = WinHttpConnect(tsSession,L"api.openai.com", 443, 0);
+
+    if(!tsConnect){
+        printf("not connecting, what the f**k is wrong wit dis huh?");
+        return;
     }
+
+    tsRequest = WinHttpOpenRequest(tsConnect, L"POST", L"/v1/chat/completions",
+                                         NULL, NULL, NULL, WINHTTP_FLAG_SECURE);
+
+    if(!tsRequest){
+        printf("im unable to create ur damn request");
+        return;
+    }
+
+    WinHttpAddRequestHeaders(tsRequest,
+         L"Content-Type: application/json\r\nAuthorization: Bearer YOUR_API_KEY\r\n",
+          -1, WINHTTP_ADDREQ_FLAG_ADD);
+
+    WinHttpSendRequest(tsRequest, NULL, 0, json_data, strlen(json_data), strlen(json_data), 0);
+}
+
+//input string - FIX to get directly from user
+char userInput[512] = "fill for users entered response";
+
+//for sending prompt to LLM
+void send_LLM(char* userMessage){
+    char json_data[1000];
+    sprintf(json_data, "{"
+        "\"model\":\"gpt-4\","
+            "\"messages\":["
+                "{\"role\":\"system\",\"content\":\"You are Tony Soprano\"},"
+                "{\"role\":\"user\",\"content\":\"%s\"}"
+            "]"
+        "}", userMessage);
+        printf("gimme a second, i gotta think.");
+        send_http_request(json_data);
+} 
+
+//for receiving the response to then go into wrapping
+void recieve_LLM(){
+
 }
 
 int main() {
@@ -32,29 +82,17 @@ int main() {
     if(!init_http()){
         return 1;
     }
-    else{
-        cleanHTTP();
-        return 0;
-    }
+    //open/done load prompt
+    printf("alright whats up, watcha got for me?!\n");
+    //create input var and catch users input
+    //store it
+    char input[512];
+    fgets(input, sizeof(input),stdin);
 
+    //send to the LLM 
+    send_LLM(input);
 
-    
-
-
+    cleanHTTP();
+    return 0;
 }
-
-void cleanHTTP(){
-    if (hRequest) WinHttpCloseHandle(hRequest);
-    if (hRequest) WinHttpCloseHandle(hRequest);
-    if (hRequest) WinHttpCloseHandle(hRequest);
-}
-//for sending prompt to LLM
-void send_LLM(){
-} 
-//for receiving the response to then go into wrapping
-void recieve_LLM(){
-
-}
-
-
 
